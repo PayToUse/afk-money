b127d6d553aa30f730889d6d8033c4ba8fa2d201
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-10-08 07:10:45 +0800

    display values for two more odometer elements

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ function makeMoney() {
   // important variables
   var money = 0;
   var display = document.querySelector("#money");
-  var display1 = document.querySelector("#rate")
+  var display1 = document.querySelector("#rate-second");
+  var display2 = document.querySelector("#rate-minute");
+  var display3 = document.querySelector("#rate-hour");
   var revenue = (Math.floor(Math.random() * 10) + 1) + (Math.random());
   
   // variables for saving
@@ -14,7 +16,9 @@ function makeMoney() {
   // odometer parameters
   display.innerHTML = newMoneyValue;
   display1.innerHTML = revenue;
-  
+  display2.innerHTML = revenue * 60;
+  display3.innerHTML = revenue * 3600; 
+
   // variables for displaying the year
   const d = new Date();
   let year = d.getFullYear();
